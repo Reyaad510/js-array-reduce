58d8c17ec98941caa6b7e714b4f70e0f3935c2dc
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-14 13:18:45 -0500

    vowelCount complete

--- reduce.js
+++ reduce.js
@@ -24,7 +24,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+  const strArr = [...str.toLowerCase()];
+  const vowels = "aeiou";
+  return strArr.reduce((acc, currentLetter) => {
+    if (vowels.indexOf(currentLetter) !== -1) {
+      if (acc[currentLetter] == null) {
+        acc[currentLetter] = 1;
+      } else {
+        acc[currentLetter]++;
+      }
+    }
+    return acc;
+  }, {});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
